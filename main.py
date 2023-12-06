f8cd128ed1ac0940454c0fe112c053f329ea9fb6
Reid Miller <rmiller14@tulane.edu>
2023-12-06 10:50:07 -0600

    update1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections import deque
 from heapq import heappush, heappop 
+import math
 
 def shortest_shortest_path(graph, source):
     """
@@ -12,11 +13,33 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
-    
 
-    
+    def ssp_recursive(past_nodes, frontier):
+        
+        if len(frontier) == 0:
+            return past_nodes
+        
+        else:
+            distance, node_edges = heappop(frontier)
+            node = node_edges[0]
+            edges = node_edges[1]
+
+            if node in past_nodes:
+                if past_nodes[node][0] == distance:
+                    if past_nodes[node][1] > edges:
+                      past_nodes[node] = (distance, edges)
+                return ssp_recursive(past_nodes, frontier)
+            
+            else:
+                past_nodes[node] = (distance, edges) 
+                for adjacent_node, weight in graph[node]:
+                    heappush(frontier, (distance + weight, (adjacent_node, edges + 1)))
+                return ssp_recursive(past_nodes,frontier)
+
+    frontier = []
+    heappush(frontier, (0, (source, 0)))
+    past_nodes = dict()
+    return ssp_recursive(past_nodes, frontier)
     
 def bfs_path(graph, source):
     """
@@ -24,8 +47,35 @@ def bfs_path(graph, source):
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+    def bp_recursive(past_nodes, frontier, path):
+        print('top')
+        print(past_nodes)
+        print(frontier)
+        print(path)
+
+        if len(frontier) == 0:
+            return path
+        
+        else:
+            past_nodes = past_nodes or frontier
+            tmp = set()
+            for node in frontier:
+                for adjacent_node in graph[node[0]]:
+                    
+                    if adjacent_node in past_nodes:
+                        pass
+                    
+                    else:
+                        path[adjacent_node] = node
+                        tmp.add(adjacent_node)
+            
+            frontier = tmp
+            return bp_recursive(past_nodes, frontier, path)
+        
+    past_nodes = set()
+    frontier = set([source])
+    path = dict()
+    return bp_recursive(past_nodes, frontier, path)
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -35,6 +85,17 @@ def get_sample_graph():
             'd': {}
             }
 
+graph = {
+              's': {('a', 1), ('c', 4)},
+              'a': {('b', 2)}, # 'a': {'b'},
+              'b': {('c', 1), ('d', 4)}, 
+              'c': {('d', 3)},
+              'd': {},
+              'e': {('d', 0)}
+          }
+bfs_path(graph, 's')
+
+
 
     
 def get_path(parents, destination):
@@ -43,6 +104,26 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    
+    def gp_recursive(result, destination):
+        
+        if destination == None:
+            return result
+        
+        else:
+            is_traceable = False
+            for parent in parents:
+                if parent == parents[destination]:
+                    destination = parent
+                    result = result + parents[destination]
+                    is_traceable = True
+            if is_traceable:
+                destination = None
+            return gp_recursive(result, destination)
+        
+    result = parents[destination]
+    return gp_recursive(result, destination)
+
+
+
 
